3e6d8514cb34dccd30419779af88a148169a6717
NMcDonald3 <flargison@yahoo.com>
2024-12-05 17:33:55 -0800

    a

---
+++ JS/transition.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const links = document.querySelectorAll(".Navigation-Menu-Links a");
+
+    links.forEach(link => {
+        link.addEventListener("click", event => {
+
+            // Add fade-out animation
+            const pageContent = document.getElementById("page-content");
+        });
+    });
+});
